9eeb11abe9a18c51998bdabc5179e0388a96fe13
alishalisha <alisha.ramos@voxmedia.com>
2015-08-25 22:49:10 -0400

    registry card styling and states

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,6 @@
 //= require_tree .
 
-$('.c-registry-card__cart').on('click', function() {
+$('.c-registry-card__gift').on('click', function() {
+  $(this).toggleClass('disabled');
   $(this).parent('.c-registry-card__info').next('.c-registry-card__qty').toggleClass('active');
 })
