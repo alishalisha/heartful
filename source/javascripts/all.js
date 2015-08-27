638ec4ce883c6ffdec37ff7dfa8cf30c41ae679d
alishalisha <alisha.ramos@voxmedia.com>
2015-08-26 22:11:38 -0400

    styling project mini component, buttons, forms

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,6 @@
 //= require_tree .
 
 $('.c-registry-card__gift').on('click', function() {
-  $(this).toggleClass('disabled');
+  $(this).toggleClass('c-button--disabled');
   $(this).parent('.c-registry-card__info').next('.c-registry-card__qty').toggleClass('active');
 })
